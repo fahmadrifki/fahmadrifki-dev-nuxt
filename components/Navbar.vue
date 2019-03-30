967a2fd4f74ee67749a886672e4c3004dbe0c2d5
fahmadrifki <fahmadrifki@gmail.com>
2019-03-30 19:36:05 +0700

    initial commit

---
+++ components/Navbar.vue
@@ -0,0 +1,87 @@
+<template>
+  <nav class="navbar navbar-expand-lg fixed-top fa-navbar" v-bind:class="{ 'animation': fixedOnScroll }">
+    <div class="container">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Specialties</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Profile</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Portfolio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style lang="sass" scoped>
+
+$green: #41B883
+
+.fa-navbar
+  background-color: rgba(0,0,0,0)
+  padding: 2rem 0
+
+
+.fa-navbar,
+.animation
+  transition: 0.5s
+  -webkit-transition: 0.5s
+
+
+.fa-navbar.animation
+  background-color: #fff
+  border-bottom: 1px solid #ececec
+  box-shadow: 0 3px 8px rgba(0,0,0,.1)
+  padding: 0.1rem
+  margin: 0 auto
+
+.nav-link
+  color: #333
+  padding: .5rem .95rem
+
+  &:hover
+    color: $green
+    border-bottom: 3px solid $green
+
+</style>
+
+<script>
+  export default {
+    name: 'MainNav',
+    data: function () {
+      return {
+        fixedOnScroll: false
+      }
+    },
+    methods: {
+      handleScroll () {
+        if (window.scrollY >= 100) {
+          this.fixedOnScroll = true
+        } else {
+          this.fixedOnScroll = false
+        }
+      }
+    },
+    created () {
+      if (process.browser) {
+        window.addEventListener('scroll', this.handleScroll)
+      }
+    },
+    beforeUpdate () {
+      if (process.browser) {
+        window.addEventListener('scroll', this.handleScroll)
+      }
+    }
+  }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+    <header>
+      <Navbar />
+      <div class="space-navbar"></div>
+    </header>
+    <section class="container ">
+      <div class="text-center">
+        <logo />
+        <h1 class="title">
+          fahmadrifki-dev-nuxt
+        </h1>
+        <h2 class="subtitle">
+          fahmadrifki.github.io developer tools
+        </h2>
+        
+      </div>
+    </section>
+    <section class="container ">
+      <div class="text-center">
+        <logo />
+        <h1 class="title">
+          fahmadrifki-dev-nuxt
+        </h1>
+        <h2 class="subtitle">
+          fahmadrifki.github.io developer tools
+        </h2>
+        
+      </div>
+    </section>
+    <section class="container ">
+      <div class="text-center">
+        <logo />
+        <h1 class="title">
+          fahmadrifki-dev-nuxt
+        </h1>
+        <h2 class="subtitle">
+          fahmadrifki.github.io developer tools
+        </h2>
+        
+      </div>
+    </section>
+    <section class="container ">
+      <div class="text-center">
+        <logo />
+        <h1 class="title">
+          fahmadrifki-dev-nuxt
+        </h1>
+        <h2 class="subtitle">
+          fahmadrifki.github.io developer tools
+        </h2>
+        
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import Navbar from '~/components/Navbar.vue'
+
+export default {
+  components: {
+    Logo,
+    Navbar
+  }
+}
+</script>
+
+<style>
+/* .container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}  */
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.space-navbar{
+  padding-top: 6.5rem;
+}
+</style>
